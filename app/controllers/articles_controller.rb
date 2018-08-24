f7fe872faaad4bf35a4705a118a5c284472f13a1
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-23 21:51:19 -0600

    refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :destroy, :edit]
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -19,13 +19,13 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    Article.destroy(params[:id])
+    @article.destroy
 
     redirect_to articles_path
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   private
@@ -33,4 +33,9 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :body)
     end
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
 end
