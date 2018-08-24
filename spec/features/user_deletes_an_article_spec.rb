a0f2cd6ccd98961765f1e4e4c9e634228b01993c
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-23 20:11:29 -0600

    add feature test for if user can delete an article

---
+++ spec/features/user_deletes_an_article_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe "user deletes an article" do
+  describe "they link from the show page" do
+    it "displays all articles without the deleted entry" do
+      article_1 = Article.create!(title: "Hatchet", body: "A survival in the woods story")
+      article_2 = Article.create!(title: "Z for Zachariah", body: "A zombie apocalypse")
+      article_3 = Article.create!(title: "Ready Player One", body: "A virtual reality universe.")
+
+      visit article_path(article_1)
+      click_link "Delete"
+
+      expect(current_path).to eq(articles_path)
+      expect(page).to have_content(article_2.title)
+      expect(page).to have_content(article_3.title)
+      expect(page).to_not have_content(article_1.title)
+    end
+  end 
+end
