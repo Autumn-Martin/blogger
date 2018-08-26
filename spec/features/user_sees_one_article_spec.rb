3209ef5c14e77a6f9d1a0610b01b001d6f8de211
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-25 20:58:09 -0600

    add feature test for comments and add relationship in article model

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -4,12 +4,17 @@ describe "user sees one article" do
   describe "they link from the articles index" do
     it "displays information for one article" do
       article = Article.create!(title: "Harry Potter and the Sorcerer's Stone", body: "A book about a rock.")
-
+      comment_1 = article.comments.create(author_name: "Moi", body: "This code is magic.")
+      comment_2 = article.comments.create(author_name: "Cat", body: "This code makes me want to lay on your keyboard.")
       visit articles_path
       click_link article.title
 
       expect(page).to have_content(article.title)
       expect(page).to have_content(article.body)
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
     end
   end
 end
